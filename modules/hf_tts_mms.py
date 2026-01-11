f68f3f428508b4c3ad6fb0f3ec2feef8c8cdfbe3
Ruben <rgottberg@gmail.com>
2026-01-11 18:29:32 +0100

    convert feature3 into smolagents tool using decorator approach

--- modules/hf_tts_mms.py
+++ modules/hf_tts_mms.py
@@ -7,11 +7,22 @@ Created on Fri Dec 12 22:13:34 2025
 """
 
 # import libraries
+from smolagents import tool
 from transformers import VitsModel, AutoTokenizer
 import torch
+import torchaudio
 
-def hfMMS(story):
-    checkpoint_dir = "./hf_mms-tts-eng/"
+@tool
+def create_audio(story: str) -> str:
+    """
+    This is a tool that converts text into speech. This tool must receive 
+    exactly 1 argument, which is the text of the story. 
+    It returns a speech transcription.
+
+    Args:
+        story: text of the story to be converted into speech.
+    """
+    checkpoint_dir = "../hf_mms-tts-eng/"
     model = VitsModel.from_pretrained(checkpoint_dir)
     tokenizer = AutoTokenizer.from_pretrained(checkpoint_dir)
         
@@ -19,5 +30,7 @@ def hfMMS(story):
     
     with torch.no_grad():
         output = model(**inputs).waveform
+
+    torchaudio.save("../story_audio.wav", output.squeeze(0), model.config.sampling_rate)
     
-    return output, model.config.sampling_rate 
+    return "ok"
