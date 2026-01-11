4fb31a682401e19cb047efaf773bf74e3ac04d7c
Ruben <rgottberg@gmail.com>
2026-01-11 13:45:52 +0100

    convert feature2 into smolagents tool using decorator approach

--- modules/ollama_story.py
+++ modules/ollama_story.py
@@ -7,11 +7,21 @@ Created on Wed Dec 10 09:50:03 2025
 """
 
 # import libraries
+from smolagents import tool
 from ollama import chat
 from ollama import ChatResponse
 
-def ollamaStory(checkpoint,user_prompt):
-    response: ChatResponse = chat(model=checkpoint, 
+@tool
+def createStory(user_prompt: str) -> str:
+    """
+    This is a tool that creates and returns stories for children based on user 
+    prompts. This tool must receive exactly 1 argument, which is the user prompt. 
+    This tool returns a newly-created story.
+
+    Args:
+        user_prompt: information provided by users to create a story.
+    """
+    response: ChatResponse = chat(model="gemma3:1b",
                                   messages=[
                                       {
                                           'role': 'system',
