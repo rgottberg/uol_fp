e371dc1ac662125e0ac9732c2968b2742c89dc48
Ruben <rgottberg@gmail.com>
2026-01-20 16:23:56 +0100

    basic frontend to create & play audio + corresponding server adjustments

--- djangoProject/storyApp/urls.py
+++ djangoProject/storyApp/urls.py
@@ -7,9 +7,12 @@ Created on Sun Jan 18 10:25:14 2026
 """
 
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-]
+    path("generate/", views.generate, name="generate"),
+    path("play/", views.play, name="play"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djangoProject/storyApp/views.py
+++ djangoProject/storyApp/views.py
@@ -5,24 +5,39 @@ from smolagents import CodeAgent, LiteLLMModel
 from .hf_asr_whisper import transcribe_speech
 from .ollama_story import create_story
 from .hf_tts_mms import create_audio
-
+from django.conf import settings
 
 def index(request):
-    model = LiteLLMModel(
-        model_id="ollama_chat/qwen2.5-coder:3b",
-        # model_id="ollama_chat/llama3.2:3b",
-        api_base="http://localhost:11434",
-        )
-       
-    agent = CodeAgent(
-        tools=[transcribe_speech,create_story,create_audio],
-        model=model,
-        max_steps=3
-    )
-
-    prompt = """ Create an audio story for children based on the user prompt 
-                 contained in the audio file stored at the following path: 
-                 ./3_audio_man.wav"""
-        
-    agent.run(prompt)
     return render(request, "storyApp/index.html")
+
+def generate(request):
+    if request.method == "POST":
+        model = LiteLLMModel(
+            model_id="ollama_chat/qwen2.5-coder:3b",
+            # model_id="ollama_chat/llama3.2:3b",
+            api_base="http://localhost:11434",
+            )
+           
+        agent = CodeAgent(
+            tools=[transcribe_speech,create_story,create_audio],
+            model=model,
+            max_steps=3
+        )
+
+        prompt = """ Create an audio story for children based on the user prompt 
+                     contained in the audio file stored at the following path: 
+                     ./3_audio_man.wav"""
+        
+        agent.run(prompt)
+        context = {"feedback": "Story generated"}
+        
+        return render(request, "storyApp/index.html", context)
+    
+
+def play(request):
+    if request.method == "POST":
+        filename = "story_audio.wav"
+        path = settings.MEDIA_URL
+        context = {"audio_file": path + filename}        
+        return render(request, "storyApp/play.html", context)
+    
