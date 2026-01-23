d9ba10e12b9cf225bbb7aeeddabcbaa3782a7b65
Ruben <rgottberg@gmail.com>
2026-01-23 14:11:46 +0100

    adjustment to function/tool to make it clearer for agent

--- djangoProject/storyApp/hf_tts_mms.py
+++ djangoProject/storyApp/hf_tts_mms.py
@@ -15,14 +15,16 @@ from pathlib import Path
 from django.conf import settings
 
 @tool
-def create_audio(story: str) -> str:
+def create_audio(story: str, file_path: str) -> str:
     """
     This is a tool that converts text into speech. This tool must receive 
-    exactly 1 argument, which is the text of the story. 
+    exactly 2 arguments, which is the text of the story the path to the output 
+    file. 
     It returns an audio file labelled "story_audio.wav".
 
     Args:
         story: text of the story to be converted into speech.
+        file_path: path to the output audio file.
     """
     checkpoint_dir = "./hf_mms-tts-eng/"
     model = VitsModel.from_pretrained(checkpoint_dir)
@@ -34,9 +36,9 @@ def create_audio(story: str) -> str:
         output = model(**inputs).waveform
 
     filename = "story_audio.wav"
-    path = Path(settings.MEDIA_ROOT)
+    file_path = Path(settings.MEDIA_ROOT)
 
-    torchaudio.save(path / filename,
+    torchaudio.save(file_path / filename,
                     output.squeeze(0), 
                     model.config.sampling_rate)
     
