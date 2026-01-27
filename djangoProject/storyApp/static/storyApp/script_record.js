9404895caf2028099e296b3783d82cf62494d28a
Ruben <rgottberg@gmail.com>
2026-01-27 12:49:49 +0100

    introduce crsf verification for audio prompt

--- djangoProject/storyApp/static/storyApp/script_record.js
+++ djangoProject/storyApp/static/storyApp/script_record.js
@@ -77,6 +77,9 @@ function recordSound(recorder,soundfile,button){
     button.style.backgroundColor = "red";
   }
 }
+
+const csrftoken = getCookie('csrftoken');
+
 function sendBlob(soundBlob) {
     // Now we can send the blob to a server...
     var serverUrl = 'http://127.0.0.1:8000/blob/';
@@ -86,7 +89,23 @@ function sendBlob(soundBlob) {
     
     var httpRequestOptions = {
         method: 'POST',
-        body: formData
+        body: formData,
+        headers: {'X-CSRFToken': csrftoken}
     };
     httpDo(serverUrl, httpRequestOptions);
+}
+// Django's documentation: acquiring csrf token
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
