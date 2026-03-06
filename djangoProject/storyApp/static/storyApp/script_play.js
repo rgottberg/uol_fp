00541ca342c8372817acc2afc80bd104f3c3d0d6
Ruben <rgottberg@gmail.com>
2026-03-06 10:01:27 +0100

    remove comments from spyder IDE re file generation

--- djangoProject/storyApp/static/storyApp/script_play.js
+++ djangoProject/storyApp/static/storyApp/script_play.js
@@ -1,11 +1,3 @@
-//#!/usr/bin/env python3
-//# -*- coding: utf-8 -*-
-//"""
-//Created on Tue Jan 20 11:58:51 2026
-//
-//@author: cod
-//"
-
 // pre-recorded SoundFile object (audio story)
 var preRecordedSound;
 
--- djangoProject/storyApp/static/storyApp/script_record.js
+++ djangoProject/storyApp/static/storyApp/script_record.js
@@ -1,10 +1,3 @@
-//#!/usr/bin/env python3
-//# -*- coding: utf-8 -*-
-//"""
-//Created on Tue Jan 20 11:58:51 2026
-//
-//@author: cod
-//"
 // array for playback buttons
 var buttons = [];
 
